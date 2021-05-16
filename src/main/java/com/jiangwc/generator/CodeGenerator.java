5b359a32eba8a02e1ede1e467d2046863578a49d
HappyCrayon <elitejiang2015@qq.com>
2021-05-16 11:29:11 +0800

    去掉手动输入相关代码

--- src/main/java/com/jiangwc/generator/CodeGenerator.java
+++ src/main/java/com/jiangwc/generator/CodeGenerator.java
@@ -1,8 +1,6 @@
 package com.jiangwc.generator;
 
-import com.baomidou.mybatisplus.core.exceptions.MybatisPlusException;
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.*;
@@ -12,30 +10,9 @@ import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 // 演示例子，执行 main 方法控制台输入模块表名回车自动生成对应项目目录中
 public class CodeGenerator {
-
-
-    /**
-     * <p>
-     * 读取控制台内容
-     * </p>
-     */
-    public static String scanner(String tip) {
-        Scanner scanner = new Scanner(System.in);
-        StringBuilder help = new StringBuilder();
-        help.append("请输入" + tip + "：");
-        System.out.println(help.toString());
-        if (scanner.hasNext()) {
-            String ipt = scanner.next();
-            if (StringUtils.isNotBlank(ipt)) {
-                return ipt;
-            }
-        }
-        throw new MybatisPlusException("请输入正确的" + tip + "！");
-    }
 
     public static void main(String[] args) {
         String[] includeTables = new String[]{"dbg_agent_ad"};
@@ -65,7 +42,6 @@ public class CodeGenerator {
 
         // 包配置
         PackageConfig pc = new PackageConfig();
-//        pc.setModuleName(scanner("模块名"));
 //        pc.setModuleName(moduleName);
         pc.setParent(classPackage);
         mpg.setPackageInfo(pc);
@@ -135,7 +111,6 @@ public class CodeGenerator {
 //        strategy.setSuperControllerClass("你自己的父类控制器,没有就不用设置!");
         // 写于父类中的公共字段
         strategy.setSuperEntityColumns("id");
-//        strategy.setInclude(scanner("表名，多个英文逗号分割").split(","));
         // <include> 与 <exclude> 只能配置一项！
 //        strategy.setInclude(includeTables);
 //        strategy.setExclude(excludeTables);
